495ecd01c2c9451d33ca205564449a65fc8d9440
John Lloyd A. Jasmines <jasminesj37@gmail.com>
2022-06-02 14:34:01 +0800

    Bot1 is working

--- JASMINES, TIC TAC TOE/Form1.cs
+++ JASMINES, TIC TAC TOE/Form1.cs
@@ -2,6 +2,7 @@ namespace JASMINES__TIC_TAC_TOE
 {
     public partial class Form1 : Form
     {
+        String Botvalue = "X";
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +10,18 @@ namespace JASMINES__TIC_TAC_TOE
 
         private void Bot1_Click(object sender, EventArgs e)
         {
-
+            if (Bot1.Text == "")
+            {
+                Bot1.Text = Botvalue;
+                if (Botvalue == "X")
+                {
+                    Botvalue = "O";
+                }
+                else
+                {
+                    Botvalue = "X";
+                }
+            }
         }
     }
 }
