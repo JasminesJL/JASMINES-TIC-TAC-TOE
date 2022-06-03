3ba96d667fa8f64d2b2b41b132d235d2d3d673ed
John Lloyd A. Jasmines <jasminesj37@gmail.com>
2022-06-03 17:41:57 +0800

    I change the design

--- JASMINES, TIC TAC TOE/Form1.Designer.cs
+++ JASMINES, TIC TAC TOE/Form1.Designer.cs
@@ -40,107 +40,117 @@
             this.Bot9 = new System.Windows.Forms.Button();
             this.BOTabout = new System.Windows.Forms.Button();
             this.BotReset = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BOTquit = new System.Windows.Forms.Button();
             this.BOTsoundoff = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Bot1
             // 
+            this.Bot1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot1.Location = new System.Drawing.Point(190, 93);
+            this.Bot1.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Bot1.Location = new System.Drawing.Point(305, 202);
             this.Bot1.Name = "Bot1";
-            this.Bot1.Size = new System.Drawing.Size(65, 65);
+            this.Bot1.Size = new System.Drawing.Size(80, 80);
             this.Bot1.TabIndex = 0;
-            this.Bot1.UseVisualStyleBackColor = true;
+            this.Bot1.UseVisualStyleBackColor = false;
             this.Bot1.Click += new System.EventHandler(this.Bot1_Click);
             // 
             // Bot2
             // 
+            this.Bot2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot2.Location = new System.Drawing.Point(190, 173);
+            this.Bot2.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot2.Location = new System.Drawing.Point(305, 288);
             this.Bot2.Name = "Bot2";
-            this.Bot2.Size = new System.Drawing.Size(65, 65);
+            this.Bot2.Size = new System.Drawing.Size(80, 80);
             this.Bot2.TabIndex = 1;
-            this.Bot2.UseVisualStyleBackColor = true;
+            this.Bot2.UseVisualStyleBackColor = false;
             this.Bot2.Click += new System.EventHandler(this.Bot2_Click);
             // 
             // Bot3
             // 
+            this.Bot3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot3.Location = new System.Drawing.Point(190, 253);
+            this.Bot3.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot3.Location = new System.Drawing.Point(305, 374);
             this.Bot3.Name = "Bot3";
-            this.Bot3.Size = new System.Drawing.Size(65, 65);
+            this.Bot3.Size = new System.Drawing.Size(80, 80);
             this.Bot3.TabIndex = 2;
-            this.Bot3.UseVisualStyleBackColor = true;
+            this.Bot3.UseVisualStyleBackColor = false;
             this.Bot3.Click += new System.EventHandler(this.Bot3_Click);
             // 
             // Bot4
             // 
+            this.Bot4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot4.Location = new System.Drawing.Point(273, 93);
+            this.Bot4.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot4.Location = new System.Drawing.Point(410, 202);
             this.Bot4.Name = "Bot4";
-            this.Bot4.Size = new System.Drawing.Size(65, 65);
+            this.Bot4.Size = new System.Drawing.Size(80, 80);
             this.Bot4.TabIndex = 3;
-            this.Bot4.UseVisualStyleBackColor = true;
+            this.Bot4.UseVisualStyleBackColor = false;
             this.Bot4.Click += new System.EventHandler(this.Bot4_Click);
             // 
             // Bot5
             // 
+            this.Bot5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot5.Location = new System.Drawing.Point(273, 173);
+            this.Bot5.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot5.Location = new System.Drawing.Point(410, 288);
             this.Bot5.Name = "Bot5";
-            this.Bot5.Size = new System.Drawing.Size(65, 65);
+            this.Bot5.Size = new System.Drawing.Size(80, 80);
             this.Bot5.TabIndex = 4;
-            this.Bot5.UseVisualStyleBackColor = true;
+            this.Bot5.UseVisualStyleBackColor = false;
             this.Bot5.Click += new System.EventHandler(this.Bot5_Click);
             // 
             // Bot6
             // 
+            this.Bot6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot6.Location = new System.Drawing.Point(273, 253);
+            this.Bot6.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot6.Location = new System.Drawing.Point(410, 374);
             this.Bot6.Name = "Bot6";
-            this.Bot6.Size = new System.Drawing.Size(65, 65);
+            this.Bot6.Size = new System.Drawing.Size(80, 80);
             this.Bot6.TabIndex = 5;
-            this.Bot6.UseVisualStyleBackColor = true;
+            this.Bot6.UseVisualStyleBackColor = false;
             this.Bot6.Click += new System.EventHandler(this.Bot6_Click);
             // 
             // Bot7
             // 
+            this.Bot7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot7.Location = new System.Drawing.Point(353, 93);
+            this.Bot7.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot7.Location = new System.Drawing.Point(515, 199);
             this.Bot7.Name = "Bot7";
-            this.Bot7.Size = new System.Drawing.Size(65, 65);
+            this.Bot7.Size = new System.Drawing.Size(80, 80);
             this.Bot7.TabIndex = 6;
-            this.Bot7.UseVisualStyleBackColor = true;
+            this.Bot7.UseVisualStyleBackColor = false;
             this.Bot7.Click += new System.EventHandler(this.Bot7_Click);
             // 
             // Bot8
             // 
+            this.Bot8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot8.Location = new System.Drawing.Point(353, 173);
+            this.Bot8.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot8.Location = new System.Drawing.Point(515, 288);
             this.Bot8.Name = "Bot8";
-            this.Bot8.Size = new System.Drawing.Size(65, 65);
+            this.Bot8.Size = new System.Drawing.Size(80, 80);
             this.Bot8.TabIndex = 7;
-            this.Bot8.UseVisualStyleBackColor = true;
+            this.Bot8.UseVisualStyleBackColor = false;
             this.Bot8.Click += new System.EventHandler(this.Bot8_Click);
             // 
             // Bot9
             // 
+            this.Bot9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.Bot9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Bot9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bot9.Location = new System.Drawing.Point(353, 253);
+            this.Bot9.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Bot9.Location = new System.Drawing.Point(515, 374);
             this.Bot9.Name = "Bot9";
-            this.Bot9.Size = new System.Drawing.Size(65, 65);
+            this.Bot9.Size = new System.Drawing.Size(80, 80);
             this.Bot9.TabIndex = 8;
-            this.Bot9.UseVisualStyleBackColor = true;
+            this.Bot9.UseVisualStyleBackColor = false;
             this.Bot9.Click += new System.EventHandler(this.Bot9_Click);
             // 
             // BOTabout
@@ -149,7 +159,7 @@
             this.BOTabout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BOTabout.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.BOTabout.ForeColor = System.Drawing.Color.Black;
-            this.BOTabout.Location = new System.Drawing.Point(434, 12);
+            this.BOTabout.Location = new System.Drawing.Point(807, 551);
             this.BOTabout.Name = "BOTabout";
             this.BOTabout.Size = new System.Drawing.Size(75, 27);
             this.BOTabout.TabIndex = 9;
@@ -162,7 +172,7 @@
             this.BotReset.BackColor = System.Drawing.Color.Moccasin;
             this.BotReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BotReset.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BotReset.Location = new System.Drawing.Point(353, 12);
+            this.BotReset.Location = new System.Drawing.Point(12, 45);
             this.BotReset.Name = "BotReset";
             this.BotReset.Size = new System.Drawing.Size(75, 27);
             this.BotReset.TabIndex = 10;
@@ -170,19 +180,19 @@
             this.BotReset.UseVisualStyleBackColor = false;
             this.BotReset.Click += new System.EventHandler(this.BotReset_Click);
             // 
-            // button1
+            // BOTquit
             // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(515, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 27);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Quit Game";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BOTquit.BackColor = System.Drawing.Color.Red;
+            this.BOTquit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BOTquit.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BOTquit.ForeColor = System.Drawing.Color.White;
+            this.BOTquit.Location = new System.Drawing.Point(12, 551);
+            this.BOTquit.Name = "BOTquit";
+            this.BOTquit.Size = new System.Drawing.Size(100, 27);
+            this.BOTquit.TabIndex = 11;
+            this.BOTquit.Text = "Quit Game";
+            this.BOTquit.UseVisualStyleBackColor = false;
+            this.BOTquit.Click += new System.EventHandler(this.button1_Click);
             // 
             // BOTsoundoff
             // 
@@ -197,15 +207,28 @@
             this.BOTsoundoff.UseVisualStyleBackColor = false;
             this.BOTsoundoff.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(174, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(595, 119);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "TIC TAC TOE";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(627, 418);
+            this.ClientSize = new System.Drawing.Size(894, 590);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BOTsoundoff);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BOTquit);
             this.Controls.Add(this.BotReset);
             this.Controls.Add(this.BOTabout);
             this.Controls.Add(this.Bot9);
@@ -223,6 +246,7 @@
             this.Text = "TIC TAC TOE";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -239,7 +263,8 @@
         private Button Bot9;
         private Button BOTabout;
         private Button BotReset;
-        private Button button1;
+        private Button BOTquit;
         private Button BOTsoundoff;
+        private Label label1;
     }
 }
